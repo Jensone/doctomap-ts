597f8bfafab709945c6c2831b0bdc7bb7b2e34fc
Jensone <hello@jensone.com>
2024-07-02 00:37:14 +0200

    Add doctor details display and edit functionality

--- doctomap/src/Pages/Doctors/Doctor.tsx
+++ doctomap/src/Pages/Doctors/Doctor.tsx
@@ -1,3 +1,59 @@
+import { useEffect, useState } from 'react';
+import { NavLink, useParams } from 'react-router-dom';
+import Nav from '../../Components/Nav';
+import UserIcon from '../../_partials/UserIcon';
+
 export default function Doctor() {
-  return <div>Doctor</div>
+    const { id } = useParams();
+    const [doctor, setDoctor] = useState([]);
+    const getOneDoctor = async () => {
+        try {
+            const response = await fetch(`http://localhost:3000/doctors/${id}`);
+            const data: any = await response.json();
+            setDoctor(data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        getOneDoctor();
+    }, [id]);
+
+    return (
+        <>
+            <Nav />
+            <section className="flex flex-col gap-4 p-4 max-w-lg m-auto bg-slate-50 rounded-lg justify-center items-center shadow-md">
+                <UserIcon />
+                <ul className="list-disc list-inside">
+                    <li>{doctor.name ? doctor.name : 'N/A'}</li>
+                    <li>{doctor.specialty ? doctor.specialty : 'N/A'}</li>
+                    <li>{doctor.phone ? doctor.phone : 'N/A'}</li>
+                    <li>{doctor.address ? doctor.address : 'N/A'}</li>
+                    <li>{doctor.zip ? doctor.zip : 'N/A'}</li>
+                    <li>{doctor.city ? doctor.city : 'N/A'}</li>
+                    <li>{doctor.country ? doctor.country : 'N/A'}</li>
+                    <li>
+                        <a
+                            href={doctor.url ? doctor.url : '#'}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            Prendre rdv sur Doctolib
+                        </a>
+                    </li>
+                </ul>
+            </section>
+
+            {/* TODO: fonctionnalités de modification et suppression */}
+            <div className="w-full flex justify-center mt-10 gap-5">
+                <NavLink to={'/doctor/' + id + '/edit'} className="border rounded-lg p-4 border-neutral-700 hover:bg-sky-300 text-center">
+                    Modifier
+                </NavLink>
+                <NavLink to={'/doctor/' + id + '/delete'} className="border rounded-lg p-4 border-red-700 hover:bg-red-300 text-center">
+                    Supprimer
+                </NavLink>
+            </div>
+        </>
+    );
 }
--- doctomap/src/Pages/Doctors/DoctorEdit.tsx
+++ doctomap/src/Pages/Doctors/DoctorEdit.tsx
@@ -1,3 +1,148 @@
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+
 export default function DoctorEdit() {
-  return <div>DoctorEdit</div>
+  const [doctor, setDoctor] = useState([]);
+  const { id } = useParams();
+  const Navigate = useNavigate();
+
+  useEffect(() => {
+      fetch(`http://localhost:3000/doctors/${id}`)
+        .then((response) => response.json())
+        .then((data) => setDoctor(data));
+  }, [id]);
+  
+  const editDoctor = async () => {
+      try {
+          const response = await fetch(`http://localhost:3000/doctors/${id}`, {
+              method: 'PUT',
+              body: JSON.stringify(doctor),
+              headers: {
+                  'Content-Type': 'application/json'
+              }
+          });
+          const data: any = await response.json();
+          setDoctor(data);
+      } catch (error) {
+          console.log(error);
+      }
+  };
+  return (
+    <>
+      <div className="flex flex-col gap-4 items-center justify-center h-screen p-4 max-w-xl m-auto">
+        <h1 className="text-3xl text-center">
+          Vous êtes sur le point de modifier le médecin {id}
+        </h1>
+        <form action="" className="flex flex-col gap-4 justify-center">
+          <div className="flex flex-col gap-4">
+            <label htmlFor="name">Nom</label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              value={doctor.name}
+              onChange={(e) => setDoctor({ ...doctor, name: e.target.value })}
+            />
+          </div>
+          <div className="flex flex-col gap-4">
+            <label htmlFor="specialty">Spécialité</label>
+            <input
+              type="text"
+              name="specialty"
+              id="specialty"
+              value={doctor.specialty}
+              onChange={(e) =>
+                setDoctor({ ...doctor, specialty: e.target.value })
+              }
+            />
+          </div>
+          <div className="flex flex-col gap-4">
+            <label htmlFor="phone">Téléphone</label>
+            <input
+              type="text"
+              name="phone"
+              id="phone"
+              value={doctor.phone}
+              onChange={(e) => setDoctor({ ...doctor, phone: e.target.value })}
+            />
+          </div>
+          <div className="flex flex-col gap-4">
+            <label htmlFor="email">Email</label>
+            <input
+              type="text"
+              name="email"
+              id="email"
+              value={doctor.email}
+              onChange={(e) => setDoctor({ ...doctor, email: e.target.value })}
+            />
+          </div>
+          <div className="flex flex-col gap-4">
+            <label htmlFor="address">Adresse</label>
+            <input
+              type="text"
+              name="address"
+              id="address"
+              value={doctor.address}
+              onChange={(e) =>
+                setDoctor({ ...doctor, address: e.target.value })
+              }
+            />
+          </div>
+          <div className="flex flex-col gap-4">
+            <label htmlFor="zip">Code postal</label>
+            <input
+              type="text"
+              name="zip"
+              id="zip"
+              value={doctor.zip}
+              onChange={(e) => setDoctor({ ...doctor, zip: e.target.value })}
+            />
+          </div>
+          <div className="flex flex-col gap-4">
+            <label htmlFor="city">Ville</label>
+            <input
+              type="text"
+              name="city"
+              id="city"
+              value={doctor.city}
+              onChange={(e) => setDoctor({ ...doctor, city: e.target.value })}
+            />
+          </div>
+          <div className="flex flex-col gap-4">
+            <label htmlFor="country">Pays</label>
+            <input
+              type="text"
+              name="country"
+              id="country"
+              value={doctor.country}
+              onChange={(e) =>
+                setDoctor({ ...doctor, country: e.target.value })
+              }
+            />
+          </div>
+          <div className="flex flex-col gap-4">
+            <label htmlFor="url">URL</label>
+            <input
+              type="text"
+              name="url"
+              id="url"
+              value={doctor.url}
+              onChange={(e) => setDoctor({ ...doctor, url: e.target.value })}
+            />
+          </div>
+          <div className="flex gap-4 justify-center">
+            <button
+              onClick={editDoctor}
+              className="border rounded-lg p-4 border-red-700 hover:bg-red-300 text-center"
+            >
+              Modifier
+            </button>
+            <button className="border border-gray-300 rounded-lg p-4 hover:border-sky-300 hover:bg-sky-300 text-center">
+              Annuler
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
 }
